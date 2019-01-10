ab11a03df014a4042d11407c89d0e179a0fc9296
Ben Manekin <ben.manekin@gmail.com>
2019-01-10 23:10:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,8 @@ function addElementToBeginningOfArray(a, b) {
 }
 
 function destructivelyAddElementToBeginningOfArray(a, b) {
-  return a.unshift(b);
+  a.unshift(b);
+  return a;
 }
 
 function addElementToEndOfArray(a, b) {
