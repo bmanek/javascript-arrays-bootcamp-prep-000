3ca6b55576a2c6239cd15e47f3e7bdad5a23f9c7
Ben Manekin <ben.manekin@gmail.com>
2019-01-10 22:58:58 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,45 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
+]
+
+function addElementToBeginningOfArray(a, b) {
+  var newArray = [b, ...a];
+  return newArray
+}
+
+function destructivelyAddElementToBeginningOfArray(a, b) {
+  return a.unshift(b);
+}
+
+function addElementToEndOfArray(a, b) {
+  var newArray = [...a, b];
+  return newArray
+}
+
+function destructivelyAddElementToEndOfArray(a, b) {
+  return a.push(b);
+}
+
+function accessElementInArray(a, n) {
+  return a[n];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(a) {
+  return a.shift();
+}
+
+function removeElementFromBeginningOfArray(a) {
+  newArray = a.slice(1)
+  return a;
+}
+
+function destructivelyRemoveElementFromEndOfArray(a) {
+  a.pop()
+}
+
+function removeElementFromEndOfArray(a) {
+  return a.slice(0, a.length -1);
+}
